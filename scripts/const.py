5bc58a31d1fe9c3ec8a154ce662b7a7a8e0d6d5b
o6ez9na <kikikippplo@gmail.com>
2024-10-04 19:16:27 +0300

    final

--- scripts/const.py
+++ scripts/const.py
@@ -4,3 +4,7 @@ class Constants:
     HMAC_SIZE = 32                                                              # * Размер имитовставки
     KEY_SIZE = 32                                                               # * Размер ключа
     ITERATIONS = 100000                                                         # * Количество итераций (чем их больше, тем выше криптографическая сложность)
+
+    @classmethod
+    def set_salt_size(cls, size:int):
+        cls.SALT_SIZE = size
--- scripts/scripts.py
+++ scripts/scripts.py
@@ -8,16 +8,38 @@ from cryptography.hazmat.primitives.ciphers.modes import CFB
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.ciphers.algorithms import AES256
 from scripts.const import Constants
+from ctypes import windll, Structure, c_long, byref
+from alive_progress import alive_bar
+
+
+class POINT(Structure):
+    _fields_ = [("x", c_long), ("y", c_long)]
+
+
+def queryMousePosition():
+    pt = POINT()
+    windll.user32.GetCursorPos(byref(pt))
+    return pt.x + pt.y
+
+
+def generate_salt():
+    res = 0
+    with alive_bar(600000) as bar:
+        for i in range(0, 600000):
+            res += queryMousePosition()
+            bar()
+    Constants.set_salt_size(size=len(str(res)))
+    return str(res).encode()
 
 
 # ? Генерация ключа
 def generate_key(password: str, salt: bytes) -> bytes:
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
-        length=Constants.KEY_SIZE,                                              # * Желаемая длинна ключа
-        salt=salt,                                                              # * Соль
+        length=Constants.KEY_SIZE,  # * Желаемая длинна ключа
+        salt=salt,  # * Соль
         iterations=Constants.ITERATIONS,
-        backend=default_backend(),                                              # * Значение по умолчанию
+        backend=default_backend(),  # * Значение по умолчанию
     )
     return kdf.derive(password.encode())
 
@@ -51,15 +73,19 @@ def process_file(
 # ? Шифрование файла
 def encrypt_file(input_filename: str, output_filename: str, password: str):
     try:
-        salt = os.urandom(Constants.SALT_SIZE)
+        salt = generate_salt()
         initialization_vector = os.urandom(Constants.IV_SIZE)
+
         key = generate_key(password, salt)
         plaintext = read_file(input_filename)
+
         hmac_value = hmac.new(key, plaintext, hashlib.sha256).digest()
         ciphertext = process_file(plaintext, key, initialization_vector, True)
+
         write_file(
             output_filename, salt + hmac_value + initialization_vector + ciphertext
         )
+
         print(f"Файл '{input_filename}' успешно зашифрован.")
     except Exception as e:
         print(f"Ошибка при шифровании: {e}")
@@ -86,11 +112,11 @@ def decrypt_file(input_filename: str, output_filename: str, password: str):
         key = generate_key(password, salt)
         decrypted_text = process_file(
             ciphertext, key, initialization_vector, False
-        )                                                                       # * Расшифровываем данные
+        )  # * Расшифровываем данные
         calculated_hmac = hmac.new(
             key, decrypted_text, hashlib.sha256
-        ).digest()                                                              # * Вычисление HMAC
-        if hmac.compare_digest(hmac_value, calculated_hmac):                    # * Сравнение  HMAC
+        ).digest()  # * Вычисление HMAC
+        if hmac.compare_digest(hmac_value, calculated_hmac):  # * Сравнение  HMAC
             write_file(output_filename, decrypted_text)
             print(f"Файл '{input_filename}' успешно расшифрован.")
         else:
